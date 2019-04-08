8e9a3ec16fd6a14ada1a73fa933095b693beedc0
Patryk Janicki <kontakt.janicki@gmail.com>
2019-04-08 20:05:42 +0200

    Add steady noise signal

---
+++ src/main/java/signal_processing/signals/SteadyNoise.java
@@ -0,0 +1,30 @@
+package signal_processing.signals;
+
+import signal_processing.Signal;
+import java.util.Random;
+
+public class SteadyNoise extends Signal {
+    private Random rng = new Random();
+
+    public SteadyNoise(int firstSample, int lastSample, double amplitude,
+                       double startTime, double endTime, double frequency) {
+        super(firstSample, lastSample);
+        setAmplitude(amplitude);
+        setStartTime(startTime);
+        setEndTime(endTime);
+        setFrequency(frequency);
+        updateValues();
+    }
+
+    public double getValue(double x) {
+        return rng.nextDouble() * -(2 * getAmplitude()) + getAmplitude();
+    }
+
+    public void updateValues() {
+        int samples = (int) (getEndTime() * getFrequency());
+        for (int i = getFirstSample(); i <= samples; i++) {
+            x.add((i / getFrequency()) + getStartTime());
+            y.add(getValue(0d));
+        }
+    }
+}
