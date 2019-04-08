9ee99d9d87045c71247c52c238fa326c9fa674a1
Patryk Janicki <kontakt.janicki@gmail.com>
2019-04-08 19:18:46 +0200

    Add signal parameters

--- src/main/java/model/Signal.java
+++ src/main/java/model/Signal.java
@@ -9,6 +9,10 @@ public abstract class Signal {
 
     private int firstSample;
     private int lastSample;
+    private double amplitude;
+    private double startTime;
+    private double endTime;
+    private double frequency;
 
     public Signal(int firstSample, int lastSample) {
         this.firstSample = firstSample;
@@ -19,7 +23,47 @@ public abstract class Signal {
         return firstSample;
     }
 
+    public void setFirstSample(int firstSample) {
+        this.firstSample = firstSample;
+    }
+
     public int getLastSample() {
         return lastSample;
+    }
+
+    public void setLastSample(int lastSample) {
+        this.lastSample = lastSample;
+    }
+
+    public double getAmplitude() {
+        return amplitude;
+    }
+
+    public void setAmplitude(double amplitude) {
+        this.amplitude = amplitude;
+    }
+
+    public double getStartTime() {
+        return startTime;
+    }
+
+    public void setStartTime(double startTime) {
+        this.startTime = startTime;
+    }
+
+    public double getEndTime() {
+        return endTime;
+    }
+
+    public void setEndTime(double endTime) {
+        this.endTime = endTime;
+    }
+
+    public double getFrequency() {
+        return frequency;
+    }
+
+    public void setFrequency(double frequency) {
+        this.frequency = frequency;
     }
 }
