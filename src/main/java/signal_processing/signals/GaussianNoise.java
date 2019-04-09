d2a3b187f51c29926d4bfbc68ee55364c8355666
KJachimstal <k,jachimstal@gmail.com>
2019-04-09 19:34:28 +0200

    Implements gaussianNoise method

--- src/main/java/signal_processing/signals/GaussianNoise.java
+++ src/main/java/signal_processing/signals/GaussianNoise.java
@@ -6,6 +6,8 @@ import java.util.Random;
 
 public class GaussianNoise extends Signal {
     private Random rng = new Random();
+    private boolean nextGaussian = false;
+    private double gaussian = 0, variable1 = 0, variable2 = 0, result = 0, multiplayer = 0;
 
     public GaussianNoise(int firstSample, int lastSample, double startTime,
                          double endTime, double frequency){
@@ -17,12 +19,29 @@ public class GaussianNoise extends Signal {
     }
 
     public double getValue(double x, double k) {
-        return 0;
-        //TODO: Implements getValue method
+        if (nextGaussian) {
+            nextGaussian = false;
+            return gaussian;
+        } else {
+            do {
+                variable1 = 2 * rng.nextDouble() - 1;
+                variable2 = 2 * rng.nextDouble() - 1;
+                result = (variable1 * variable1) + (variable2 * variable2);
+            } while (result >= 1 || result == 0);
+            multiplayer = StrictMath.sqrt(-2 * StrictMath.log(result) / result);
+            gaussian = variable2 * multiplayer;
+            nextGaussian = true;
+            return variable1 * multiplayer;
+        }
     }
 
     public void updateValues() {
-        //TODO: implements updateValues method
+        int samples = (int) (getFrequency() * getEndTime());
+        for (int i = getFirstSample(); i <= samples; i++) {
+            double t = i / getFrequency() + getStartTime();
+            x.add(t);
+            y.add(getValue(t,0));
+        }
 
     }
 }
