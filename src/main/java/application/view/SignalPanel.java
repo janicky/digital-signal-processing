5fb97f2fe96040b06f1c05c03bc9e53a2d3a4f5d
Patryk Janicki <kontakt.janicki@gmail.com>
2019-04-10 16:24:18 +0200

    Add first and last sample spinner

--- src/main/java/application/view/SignalPanel.java
+++ src/main/java/application/view/SignalPanel.java
@@ -7,13 +7,30 @@ import javax.swing.*;
 public class SignalPanel extends JPanel {
     private JLabel signalName;
     public JPanel signalPanel;
-    private JComboBox comboBox;
+    private JComboBox signalType;
+    private JSpinner firstSample;
+    private JSpinner lastSample;
+    private SpinnerNumberModel lastSampleModel;
 
     public SignalPanel() {
         initializeComboBox();
     }
 
     private void initializeComboBox() {
-        comboBox.setModel(new DefaultComboBoxModel(Signal.getSignals()));
+        signalType.setModel(new DefaultComboBoxModel(Signal.getSignals()));
+        firstSample.setModel(new SpinnerNumberModel(1, 1, Integer.MAX_VALUE, 1));
+        firstSample.addChangeListener(e -> onFirstSampleChange());
+        lastSampleModel = new SpinnerNumberModel(2, 2, Integer.MAX_VALUE, 1);
+        lastSample.setModel(lastSampleModel);
+    }
+
+//    Actions
+    private void onFirstSampleChange() {
+        int lastSampleValue = (int) lastSample.getValue();
+        int firstSampleValue = (int) firstSample.getValue();
+        if (lastSampleValue < firstSampleValue + 1) {
+            lastSampleModel.setMinimum(firstSampleValue + 1);
+            lastSample.setValue(firstSampleValue + 1);
+        }
     }
 }
