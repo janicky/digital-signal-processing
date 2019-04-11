b3c34e156849d608411471732ea19875a9b3ece5
Patryk Janicki <kontakt.janicki@gmail.com>
2019-04-12 00:33:58 +0200

    Update operations class

--- src/main/java/signal_processing/helpers/Operations.java
+++ src/main/java/signal_processing/helpers/Operations.java
@@ -10,7 +10,9 @@ import java.util.List;
 public class Operations {
 
     public static ISignal getMultiplySignals(ISignal signal1, ISignal signal2) {
-        ISignal calculatedSignal = new GeneratedSignal();
+        GeneratedSignal calculatedSignal = new GeneratedSignal();
+        List<Double> tempX = new ArrayList<>();
+        List<Double> tempY = new ArrayList<>();
         List<Double> tempList = new ArrayList<>();
         int count1 = 0;
         int count2 = 0;
@@ -18,15 +20,15 @@ public class Operations {
         for (int i = 0; i < signal1.getValuesX().size(); i++) {
             for (int j = 0; j < signal2.getValuesY().size(); j++) {
                 if (signal1.getValuesX().get(i) == signal2.getValuesX().get(j)) {
-                    calculatedSignal.getValuesX().add(signal1.getValuesX().get(i));
-                    calculatedSignal.getValuesY().add(signal1.getValuesY().get(i) * signal2.getValuesY().get(j));
+                    tempX.add(signal1.getValuesX().get(i));
+                    tempY.add(signal1.getValuesY().get(i) * signal2.getValuesY().get(j));
                     count1++;
                     tempList.add((double) j);
                 }
             }
             if (count1 == 0) {
-                calculatedSignal.getValuesX().add(signal1.getValuesX().get(i));
-                calculatedSignal.getValuesY().add(0d);
+                tempX.add(signal1.getValuesX().get(i));
+                tempY.add(0d);
             }
             count1 = 0;
 
@@ -39,33 +41,38 @@ public class Operations {
                 }
             }
             if (count2 == 0) {
-                calculatedSignal.getValuesX().add(signal2.getValuesX().get(k));
-                calculatedSignal.getValuesY().add(0d);
+                tempX.add(signal2.getValuesX().get(k));
+                tempY.add(0d);
             }
             count2 = 0;
         }
 
+        calculatedSignal.setValuesX(tempX);
+        calculatedSignal.setValuesY(tempY);
         return calculatedSignal;
     }
 
     public static ISignal getAddSignals(ISignal signal1, ISignal signal2) {
-        ISignal calculatedSignal = new GeneratedSignal();
+        GeneratedSignal calculatedSignal = new GeneratedSignal();
+        List<Double> tempX = new ArrayList<>();
+        List<Double> tempY = new ArrayList<>();
         List<Double> tempList = new ArrayList<>();
+
         int count1 = 0;
         int count2 = 0;
 
         for (int i = 0; i < signal1.getValuesX().size(); i++) {
             for (int j = 0; j < signal2.getValuesX().size(); j++) {
                 if (signal1.getValuesX().get(i) == signal2.getValuesX().get(i)) {
-                    calculatedSignal.getValuesX().add(signal1.getValuesX().get(i));
-                    calculatedSignal.getValuesY().add(signal1.getValuesY().get(i) + signal2.getValuesY().get(j));
+                    tempX.add(signal1.getValuesX().get(i));
+                    tempY.add(signal1.getValuesY().get(i) + signal2.getValuesY().get(j));
                     count1++;
                     tempList.add((double) j);
                 }
             }
             if (count1 == 0) {
-                calculatedSignal.getValuesX().add(signal1.getValuesX().get(i));
-                calculatedSignal.getValuesY().add(signal1.getValuesY().get(i));
+                tempX.add(signal1.getValuesX().get(i));
+                tempY.add(signal1.getValuesY().get(i));
             }
             count1 = 0;
         }
@@ -77,17 +84,21 @@ public class Operations {
                 }
             }
             if (count2 == 0) {
-                calculatedSignal.getValuesX().add(signal2.getValuesX().get(k));
-                calculatedSignal.getValuesY().add(signal2.getValuesY().get(k));
+                tempX.add(signal2.getValuesX().get(k));
+                tempY.add(signal2.getValuesY().get(k));
             }
             count2 = 0;
         }
 
+        calculatedSignal.setValuesX(tempX);
+        calculatedSignal.setValuesY(tempY);
         return calculatedSignal;
     }
 
     public static ISignal getDivideSignals(ISignal signal1, ISignal signal2) {
-        ISignal calculatedSignal = new GeneratedSignal();
+        GeneratedSignal calculatedSignal = new GeneratedSignal();
+        List<Double> tempX = new ArrayList<>();
+        List<Double> tempY = new ArrayList<>();
         List<Double> tempList = new ArrayList<>();
         int count1 = 0;
         int count2 = 0;
@@ -96,16 +107,16 @@ public class Operations {
             for (int j = 0; j < signal2.getValuesX().size(); j++) {
                 if (signal1.getValuesX().get(i) == signal2.getValuesX().get(j)) {
                     if (signal2.getValuesY().get(j) != 0) {
-                        calculatedSignal.getValuesX().add(signal1.getValuesX().get(i));
-                        calculatedSignal.getValuesY().add(signal1.getValuesY().get(i) / signal2.getValuesY().get(j));
+                        tempX.add(signal1.getValuesX().get(i));
+                        tempY.add(signal1.getValuesY().get(i) / signal2.getValuesY().get(j));
                     }
                 }
                 count1++;
                 tempList.add((double) j);
             }
             if (count1 == 0) {
-                calculatedSignal.getValuesX().add(signal1.getValuesX().get(i));
-                calculatedSignal.getValuesY().add(0d);
+                tempX.add(signal1.getValuesX().get(i));
+                tempY.add(0d);
             }
             count1 = 0;
         }
@@ -117,17 +128,21 @@ public class Operations {
                 }
             }
             if ( count2 == 0) {
-                calculatedSignal.getValuesX().add(signal2.getValuesX().get(k));
-                calculatedSignal.getValuesY().add(0d);
+                tempX.add(signal2.getValuesX().get(k));
+                tempY.add(0d);
             }
             count2 = 0;
         }
 
+        calculatedSignal.setValuesX(tempX);
+        calculatedSignal.setValuesY(tempY);
         return calculatedSignal;
     }
 
     public static ISignal getSubstractSignals(ISignal signal1, ISignal signal2) {
-        ISignal calculatedSignal = new GeneratedSignal();
+        GeneratedSignal calculatedSignal = new GeneratedSignal();
+        List<Double> tempX = new ArrayList<>();
+        List<Double> tempY = new ArrayList<>();
         List<Double> tempList = new ArrayList<>();
         int count1 = 0;
         int count2 = 0;
@@ -135,15 +150,15 @@ public class Operations {
         for (int i = 0; i < signal1.getValuesX().size(); i++) {
             for (int j = 0; j < signal2.getValuesX().size(); j++) {
                 if (signal1.getValuesX().get(i) == signal2.getValuesX().get(i)) {
-                    calculatedSignal.getValuesX().add(signal1.getValuesX().get(i));
-                    calculatedSignal.getValuesY().add(signal1.getValuesY().get(i) - signal2.getValuesY().get(j));
+                    tempX.add(signal1.getValuesX().get(i));
+                    tempY.add(signal1.getValuesY().get(i) - signal2.getValuesY().get(j));
                     count1++;
                     tempList.add((double) j);
                 }
             }
             if (count1 == 0) {
-                calculatedSignal.getValuesX().add(signal1.getValuesX().get(i));
-                calculatedSignal.getValuesY().add(signal1.getValuesY().get(i));
+                tempX.add(signal1.getValuesX().get(i));
+                tempY.add(signal1.getValuesY().get(i));
             }
             count1 = 0;
         }
@@ -155,12 +170,14 @@ public class Operations {
                 }
             }
             if (count2 == 0) {
-                calculatedSignal.getValuesX().add(signal2.getValuesX().get(k));
-                calculatedSignal.getValuesY().add( - signal2.getValuesY().get(k));
+                tempX.add(signal2.getValuesX().get(k));
+                tempY.add( - signal2.getValuesY().get(k));
             }
             count2 = 0;
         }
 
+        calculatedSignal.setValuesX(tempX);
+        calculatedSignal.setValuesY(tempY);
         return calculatedSignal;
     }
 
