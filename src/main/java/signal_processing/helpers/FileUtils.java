ca5e14ec74b0ecf77adca46a1fbb6a1855d59b1f
Patryk Janicki <kontakt.janicki@gmail.com>
2019-04-12 17:43:56 +0200

    Add load signal method to file utils class

--- src/main/java/signal_processing/helpers/FileUtils.java
+++ src/main/java/signal_processing/helpers/FileUtils.java
@@ -1,17 +1,22 @@
 package signal_processing.helpers;
 
 import signal_processing.ISignal;
+import signal_processing.signals.GeneratedSignal;
 
-import java.io.DataOutputStream;
-import java.io.FileOutputStream;
-import java.io.IOException;
+import java.io.*;
+import java.util.ArrayList;
 import java.util.List;
 
 public class FileUtils {
+
+    public static final int CONTROL_NUMBER_START = 98172419;
+    public static final int CONTROL_NUMBER_END = 129837128;
+
     public static void saveSignal(ISignal signal, String filename) throws IOException {
         FileOutputStream fos = new FileOutputStream(filename);
         DataOutputStream dos = new DataOutputStream(fos);
 
+        dos.writeInt(CONTROL_NUMBER_START);
         dos.writeDouble(signal.getStartTime());
         dos.writeDouble(signal.getFrequency());
         dos.writeInt(0); // value type
@@ -28,6 +33,44 @@ public class FileUtils {
             dos.writeDouble(d);
         }
 
+        dos.writeInt(CONTROL_NUMBER_END);
         dos.close();
+    }
+
+    public static ISignal loadSignal(String filename) throws IOException {
+        FileInputStream fis = new FileInputStream(filename);
+        DataInputStream dis = new DataInputStream(fis);
+
+        int control_start = dis.readInt();
+        if (control_start != CONTROL_NUMBER_START) {
+            throw new IOException();
+        }
+
+        double startTime = dis.readDouble();
+        double frequency = dis.readDouble();
+        int type = dis.readInt();
+
+        int count = dis.readInt();
+
+        List<Double> x = new ArrayList<>();
+        List<Double> y = new ArrayList<>();
+
+        for (int i = 0; i < count; i++) {
+            x.add(dis.readDouble());
+        }
+        for (int i = 0; i < count; i++) {
+            y.add(dis.readDouble());
+        }
+
+        int control_end = dis.readInt();
+        if (control_end != CONTROL_NUMBER_END) {
+            throw new IOException();
+        }
+
+        GeneratedSignal signal = new GeneratedSignal();
+        signal.setValuesX(x);
+        signal.setValuesY(y);
+
+        return signal;
     }
 }
