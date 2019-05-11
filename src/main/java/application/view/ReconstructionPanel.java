6b1334c8ac4ce125a556ee84dc15cb227afbd35b
Patryk Janicki <kontakt.janicki@gmail.com>
2019-05-11 21:30:53 +0200

    Add listeners in reconstruction panel class

--- src/main/java/application/view/ReconstructionPanel.java
+++ src/main/java/application/view/ReconstructionPanel.java
@@ -1,11 +1,54 @@
 package application.view;
 
 import javax.swing.*;
+import javax.swing.event.ChangeListener;
+import java.awt.event.ActionListener;
 
 public class ReconstructionPanel {
     private JPanel mainPanel;
+    private JComboBox reconstructionSignal;
+    private JSpinner reconstrunctionFrequency;
+    private JButton setAsSignal1Button;
+    private JButton setAsSignal2Button;
+    private JButton exportButton;
+    private JButton previewButton;
+    private JPanel signalPanel;
+    private JLabel noSignal;
+    private JRadioButton extrapolationRadioButton;
+    private JRadioButton interpolationRadioButton;
+    private JRadioButton sincRadioButton;
 
     public JPanel getMainPanel() {
         return mainPanel;
+    }
+
+    public void addReconstructionFrequencyListener(ChangeListener listener) {
+        reconstrunctionFrequency.addChangeListener(listener);
+    }
+
+    public void addReconstructionSignalListener(ActionListener listener) {
+        reconstructionSignal.addActionListener(listener);
+    }
+
+    public void addSetAsSignal1ButtonListener(ActionListener listener) {
+        setAsSignal1Button.addActionListener(listener);
+    }
+
+    public void addSetAsSignal2ButtonListener(ActionListener listener) {
+        setAsSignal2Button.addActionListener(listener);
+    }
+
+    public void addExportButtonListener(ActionListener listener) {
+        exportButton.addActionListener(listener);
+    }
+
+    public void addPreviewButtonListener(ActionListener listener) {
+        previewButton.addActionListener(listener);
+    }
+
+    public void addRadioButtonListener(ActionListener listener) {
+        extrapolationRadioButton.addActionListener(listener);
+        interpolationRadioButton.addActionListener(listener);
+        sincRadioButton.addActionListener(listener);
     }
 }
